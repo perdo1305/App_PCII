0bfd9eb931aa563a42fc57bb1614085e85d78248
perdo1305 <pedroandre.sferreira@gmail.com>
2023-12-24 15:52:27 +0000

    try to do unit tests (failed)

---
+++ Test/GestaoTest.java
@@ -0,0 +1,82 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GestaoTest {
+    private Gestao gestao;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        gestao = new Gestao();
+    }
+
+    @org.junit.jupiter.api.Test
+    void criarVeiculo() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void procurarVeiculo() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void consultarVeiculo() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void criarCliente() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void consultarCliente() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void procurarCliente() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void alterarDadosCliente() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removerCliente() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void criarPostoCarregamento() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void consultarPostoCarregamento() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void procurarPosto() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void registrarPagamento() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void consultarPagamentoPorSessao() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void menuregistarSessaoCarregamento() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void menuConsultarSessaoCarregamento() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void registarSessaoCarregamento() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void consultarSessaoCarregamento() {
+    }
+}
